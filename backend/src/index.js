c3066eec61dce06c76932f8afa373ffba39f5134
wwwbrunagon <wwwbrunagon@gmail.com>
2020-06-30 04:29:32 -0300

    back-end setup tests

---
+++ backend/src/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/projects', (req, res) =>{
+    return res.json({message: 'Hello World'})
+})
+
+app.listen(3333, () => {
+    console.log(' 🚀 Backend started!')
+})
