7f8a3252e1457327bc49f246ac1f52e90110d5b6
wwwbrunagon <wwwbrunagon@gmail.com>
2020-09-21 22:41:35 -0300

    Query params at insomnia

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,9 +2,61 @@ const express = require('express')
 
 const app = express()
 
-app.get('/projects', (req, res) =>{
-    return res.json({message: 'Hello World'})
-})
+/**
+* HTTP Methods
+* GET: Buscar informação do back-end
+* POST: Criar uma informação no back-end
+* PUT/PATCH: Alterar uma informação no back-end
+* DELETE:  Deletar uma informação no back-end
+*/
+
+/**
+* Tipos de paramêtros:
+* Query Paramns: Filtros e paginação / normalmente tipo GET
+* Routes Params:
+* Request Body:
+*/
+
+
+app.get('/projects', (req, res) => {
+    const {title, owner} = req.query;
+    console.log(title);
+    console.log(owner);
+
+    return res.json([
+        'Project 01',
+        'Project 02'
+    ])
+});
+
+
+app.post('/projects', (req, res) => {
+    return res.json([
+        'Project 01',
+        'Project 02',
+        'Project 03'
+    ])
+});
+
+app.put('/projects/:id ', (req, res) => {
+    return res.json([
+        'Project 04',
+        'Project 02',
+        'Project 03'
+    ])
+});
+
+
+app.delete('/projects/:id ', (req, res) => {
+    return res.json([
+        'Project 01',
+        'Project 02',
+        'Project 03'
+    ])
+});
+
+
+
 
 app.listen(3333, () => {
     console.log(' 🚀 Backend started!')
