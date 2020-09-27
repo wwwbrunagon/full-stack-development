ede332598f0bcd7550e4db95ebcab1d4d4ddc521
wwwbrunagon <wwwbrunagon@gmail.com>
2020-09-27 17:44:55 -0300

    introduction to functional application

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,63 +1,74 @@
-const express = require('express')
+const express = require('express');
 
-const app = express()
+const app = express();
 
-/**
-* HTTP Methods
-* GET: Buscar informação do back-end
-* POST: Criar uma informação no back-end
-* PUT/PATCH: Alterar uma informação no back-end
-* DELETE:  Deletar uma informação no back-end
-*/
+app.use(express.json());
 
 /**
-* Tipos de paramêtros:
-* Query Paramns: Filtros e paginação / normalmente tipo GET
-* Routes Params:
-* Request Body:
-*/
+ * Métodos HTTP:
+ * 
+ * GET: Buscar informações do back-end
+ * POST: Criar uma informação no back-end
+ * PUT/PATCH: Alterar uma informação no back-end
+ * DELETE: Deletar uma informação no back-end
+ */
+
+/**
+ * Tipos de parâmetros:
+ * 
+ * Query Params: Filtros e paginação
+ * Route Params: Identificar recursos (Atualizar/Deletar)
+ * Request Body: Conteúdo na hora criar ou editar um recurso (JSON)
+ */
+
 
 
 app.get('/projects', (req, res) => {
-    const {title, owner} = req.query;
-    console.log(title);
-    console.log(owner);
+  const { title, owner } = req.query;
+  console.log(title);
+  console.log(owner);
 
-    return res.json([
-        'Project 01',
-        'Project 02'
-    ])
+  return res.json([
+    'Project 01',
+    'Project 02'
+  ])
 });
 
 
 app.post('/projects', (req, res) => {
-    return res.json([
-        'Project 01',
-        'Project 02',
-        'Project 03'
-    ])
+
+  const body = req.body;
+  
+  console.log(body)
+
+  
+  return res.json([
+    'Project 01',
+    'Project 02',
+    'Project 03'
+  ])
 });
 
-app.put('/projects/:id ', (req, res) => {
-    return res.json([
-        'Project 04',
-        'Project 02',
-        'Project 03'
-    ])
+app.put('/projects/:id', (req, res) => {
+  return res.json([
+    'Project 04',
+    'Project 02',
+    'Project 03'
+  ])
 });
 
 
-app.delete('/projects/:id ', (req, res) => {
-    return res.json([
-        'Project 01',
-        'Project 02',
-        'Project 03'
-    ])
+app.delete('/projects/:id', (req, res) => {
+  return res.json([
+    'Project 01',
+    'Project 02',
+    'Project 03'
+  ])
 });
 
 
 
 
 app.listen(3333, () => {
-    console.log(' 🚀 Backend started!')
+  console.log(' 🚀 Backend started!')
 })
